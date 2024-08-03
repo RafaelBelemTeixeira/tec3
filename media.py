fed23ea7c7d8fef00fbf6f6d3b84ea91723d8d60
Rafael <rbteixeira@inf.ufpel.edu.br>
2024-08-03 20:47:54 -0300

    media.py

---
+++ media.py
@@ -0,0 +1,11 @@
+# Solicita ao usuário que insira a primeira nota
+nota1 = float(input("Digite a primeira nota: "))
+
+# Solicita ao usuário que insira a segunda nota
+nota2 = float(input("Digite a segunda nota: "))
+
+# Calcula a média das duas notas
+media = (nota1 + nota2) / 2
+
+# Exibe a média calculada
+print(f"A média das notas é: {media}")
